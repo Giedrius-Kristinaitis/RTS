a4d46255eb872dc465a06d3445719e42b2fdb21e
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-05-13 21:22:15 +0300

    Created map abstraction

--- core/src/com/gasis/rts/logic/map/Map.java
+++ core/src/com/gasis/rts/logic/map/Map.java
@@ -1,4 +1,19 @@
 package com.gasis.rts.logic.map;
 
-public class Map {
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.gasis.rts.resources.Resources;
+
+/**
+ * Game map abstraction
+ */
+public interface Map {
+
+    /**
+     * Renders the map to the screen
+     *
+     * @param batch sprite batch to draw the map to
+     * @param res object used to access assets
+     * @param delta time elapsed since the last render
+     */
+    void render(SpriteBatch batch, Resources res, float delta);
 }
