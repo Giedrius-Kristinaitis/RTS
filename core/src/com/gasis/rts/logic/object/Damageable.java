9e4db5368fbcabfaa5d98f2fc50e81427ebf0a32
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-05-24 19:51:15 +0300

    Created base unit class

--- core/src/com/gasis/rts/logic/object/Damageable.java
+++ core/src/com/gasis/rts/logic/object/Damageable.java
@@ -1,4 +1,15 @@
 package com.gasis.rts.logic.object;
 
+/**
+ * Indicates that whatever implements this interface can take damage
+ */
 public interface Damageable {
+
+    /**
+     * Does damage to the object
+     *
+     * @param attack attack stat of the attacker,
+     *               damage will be calculated based on the object's defence
+     */
+    void doDamage(float attack);
 }
--- core/src/com/gasis/rts/logic/object/unit/Unit.java
+++ core/src/com/gasis/rts/logic/object/unit/Unit.java
@@ -1,4 +1,104 @@
 package com.gasis.rts.logic.object.unit;
 
-public class Unit {
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.gasis.rts.logic.object.GameObject;
+import com.gasis.rts.resources.Resources;
+import com.gasis.rts.utils.Constants;
+
+import java.util.List;
+
+/**
+ * Represents a single unit on a map
+ */
+public abstract class Unit extends GameObject {
+
+    // unit facing directions
+    public static final byte NORTH = 0;
+    public static final byte NORTH_EAST = 1;
+    public static final byte EAST = 2;
+    public static final byte SOUTH_EAST = 3;
+    public static final byte SOUTH = 4;
+    public static final byte SOUTH_WEST = 5;
+    public static final byte WEST = 6;
+    public static final byte NORTH_WEST = 7;
+
+    // textures used by the unit
+    protected List<String> textures;
+
+    // the index of the current texture in the texture list
+    protected byte currentTexture;
+
+    // the direction the unit is currently facing
+    protected byte facingDirection;
+
+    /**
+     * Sets the direction the unit is facing
+     *
+     * @param facingDirection new direction the unit is facing
+     */
+    public void setFacingDirection(byte facingDirection) {
+        this.facingDirection = facingDirection;
+
+        currentTexture = facingDirection;
+    }
+
+    /**
+     * Gets the direction the unit is facing
+     * @return
+     */
+    public byte getFacingDirection() {
+        return facingDirection;
+    }
+
+    /**
+     * Sets the textures of the unit
+     *
+     * @param textures new texture list
+     */
+    public void setTextures(List<String> textures) {
+        this.textures = textures;
+    }
+
+    /**
+     * Gets the textures of the unit
+     * @return
+     */
+    public Iterable<String> getTextures() {
+        return textures;
+    }
+
+    /**
+     * Sets the current texture index
+     *
+     * @param currentTexture new texture index
+     */
+    public void setCurrentTexture(byte currentTexture) {
+        this.currentTexture = currentTexture;
+    }
+
+    /**
+     * Gets the index of the current unit's texture
+     *
+     * @return
+     */
+    public byte getCurrentTexture() {
+        return currentTexture;
+    }
+
+    /**
+     * Renders the object to the screen
+     *
+     * @param batch     sprite batch to draw to
+     * @param resources game assets
+     */
+    @Override
+    public void render(SpriteBatch batch, Resources resources) {
+        batch.draw(
+                resources.atlas(Constants.FOLDER_ATLASES + atlas).findRegion(textures.get(currentTexture)),
+                x,
+                y,
+                width,
+                height
+        );
+    }
 }
