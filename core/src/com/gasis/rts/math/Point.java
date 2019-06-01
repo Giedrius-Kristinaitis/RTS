4961368686744bd9c95d1c685779e6f8b8841a16
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-06-01 21:14:38 +0300

    Created a 2D point representation

--- core/src/com/gasis/rts/math/Point.java
+++ core/src/com/gasis/rts/math/Point.java
@@ -1,4 +1,27 @@
 package com.gasis.rts.math;
 
+/**
+ * Represents a 2D point
+ */
 public class Point {
+
+    // coordinates of the point
+    public float x;
+    public float y;
+
+    /**
+     * Default class constructor
+     */
+    public Point() {}
+
+    /**
+     * Constructor with arguments
+     *
+     * @param x x coordinate
+     * @param y y coordinate
+     */
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
 }
