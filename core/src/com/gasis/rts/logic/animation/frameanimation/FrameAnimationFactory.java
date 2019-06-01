fd1db6b60645333ff06c5be3b63b8316ec776e86
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-06-02 00:43:41 +0300

    Finished fire source class

--- core/src/com/gasis/rts/logic/animation/frameanimation/FrameAnimationFactory.java
+++ core/src/com/gasis/rts/logic/animation/frameanimation/FrameAnimationFactory.java
@@ -1,8 +1,6 @@
 package com.gasis.rts.logic.animation.frameanimation;
 
-import com.badlogic.gdx.Gdx;
 import com.gasis.rts.logic.animation.AnimationFactory;
-import com.gasis.rts.utils.Constants;
 
 /**
  * Creates frame animations
@@ -11,33 +9,51 @@ public class FrameAnimationFactory implements AnimationFactory {
 
     // ***** ANIMATION ID VALUES ***** //
     public static final short ID_WHITE_SMOKE_BALL = 0;
-    public static final short ID_ONE_TANK_GUN_SHELL_FIRE_R = 1;
-    public static final short ID_ONE_TANK_GUN_SHELL_FIRE_TR = 2;
-    public static final short ID_ONE_TANK_GUN_SHELL_FIRE_T = 3;
-    public static final short ID_ONE_TANK_GUN_SHELL_FIRE_TL = 4;
-    public static final short ID_ONE_TANK_GUN_SHELL_FIRE_L = 5;
-    public static final short ID_ONE_TANK_GUN_SHELL_FIRE_BL = 6;
-    public static final short ID_ONE_TANK_GUN_SHELL_FIRE_B = 7;
-    public static final short ID_ONE_TANK_GUN_SHELL_FIRE_BR = 8;
+    public static final short ID_ONE_TANK_GUN_SHELL_FIRE_EAST = 1;
+    public static final short ID_ONE_TANK_GUN_SHELL_FIRE_NORTH_EAST = 2;
+    public static final short ID_ONE_TANK_GUN_SHELL_FIRE_NORTH = 3;
+    public static final short ID_ONE_TANK_GUN_SHELL_FIRE_NORTH_WEST = 4;
+    public static final short ID_ONE_TANK_GUN_SHELL_FIRE_WEST = 5;
+    public static final short ID_ONE_TANK_GUN_SHELL_FIRE_SOUTH_WEST = 6;
+    public static final short ID_ONE_TANK_GUN_SHELL_FIRE_SOUTH = 7;
+    public static final short ID_ONE_TANK_GUN_SHELL_FIRE_SOUTH_EAST = 8;
     public static final short ID_PROJECTILE_EXPLOSION = 9;
-    public static final short ID_TANK_PROJECTILE = 10;
+    public static final short ID_SHELL = 10;
     public static final short ID_MISSILE = 11;
     public static final short ID_MISSILE_TRAIL = 12;
     public static final short ID_MISSILE_LAUNCH = 13;
+    public static final short ID_BULLET = 14;
+    public static final short ID_BULLET_FIRE_NORTH = 15;
+    public static final short ID_BULLET_FIRE_NORTH_EAST = 16;
+    public static final short ID_BULLET_FIRE_EAST = 17;
+    public static final short ID_BULLET_FIRE_SOUTH_EAST = 18;
+    public static final short ID_BULLET_FIRE_SOUTH = 19;
+    public static final short ID_BULLET_FIRE_SOUTH_WEST = 20;
+    public static final short ID_BULLET_FIRE_WEST = 21;
+    public static final short ID_BULLET_FIRE_NORTH_WEST = 22;
+    public static final short ID_BULLET_RICOCHET = 23;
+    public static final short ID_TWO_TANK_GUN_SHELL_FIRE_EAST = 24;
+    public static final short ID_TWO_TANK_GUN_SHELL_FIRE_NORTH_EAST = 25;
+    public static final short ID_TWO_TANK_GUN_SHELL_FIRE_NORTH = 26;
+    public static final short ID_TWO_TANK_GUN_SHELL_FIRE_NORTH_WEST = 27;
+    public static final short ID_TWO_TANK_GUN_SHELL_FIRE_WEST = 28;
+    public static final short ID_TWO_TANK_GUN_SHELL_FIRE_SOUTH_WEST = 29;
+    public static final short ID_TWO_TANK_GUN_SHELL_FIRE_SOUTH = 30;
+    public static final short ID_TWO_TANK_GUN_SHELL_FIRE_SOUTH_EAST = 31;
     // ***** END OF ANIMATION ID ***** //
 
     // ***** ANIMATION LOADERS ***** //
     private static final FrameAnimationLoader whiteSmokeBallLoader = new FrameAnimationLoader();
-    private static final FrameAnimationLoader oneTankGunShellFireR_loader = new FrameAnimationLoader();
-    private static final FrameAnimationLoader oneTankGunShellFireTR_loader = new FrameAnimationLoader();
-    private static final FrameAnimationLoader oneTankGunShellFireT_loader = new FrameAnimationLoader();
-    private static final FrameAnimationLoader oneTankGunShellFireTL_loader = new FrameAnimationLoader();
-    private static final FrameAnimationLoader oneTankGunShellFireL_loader = new FrameAnimationLoader();
-    private static final FrameAnimationLoader oneTankGunShellFireBL_loader = new FrameAnimationLoader();
-    private static final FrameAnimationLoader oneTankGunShellFireB_loader = new FrameAnimationLoader();
-    private static final FrameAnimationLoader oneTankGunShellFireBR_loader = new FrameAnimationLoader();
+    private static final FrameAnimationLoader oneTankGunShellFireEast_loader = new FrameAnimationLoader();
+    private static final FrameAnimationLoader oneTankGunShellFireNorthEast_loader = new FrameAnimationLoader();
+    private static final FrameAnimationLoader oneTankGunShellFireNorth_loader = new FrameAnimationLoader();
+    private static final FrameAnimationLoader oneTankGunShellFireNorthWest_loader = new FrameAnimationLoader();
+    private static final FrameAnimationLoader oneTankGunShellFireWest_loader = new FrameAnimationLoader();
+    private static final FrameAnimationLoader oneTankGunShellFireSouthWest_loader = new FrameAnimationLoader();
+    private static final FrameAnimationLoader oneTankGunShellFireSouth_loader = new FrameAnimationLoader();
+    private static final FrameAnimationLoader oneTankGunShellFireSouthEast_loader = new FrameAnimationLoader();
     private static final FrameAnimationLoader projectileExplosionLoader = new FrameAnimationLoader();
-    private static final FrameAnimationLoader tankProjectileLoader = new FrameAnimationLoader();
+    private static final FrameAnimationLoader shellLoader = new FrameAnimationLoader();
     private static final FrameAnimationLoader missileLoader = new FrameAnimationLoader();
     private static final FrameAnimationLoader missileTrailLoader = new FrameAnimationLoader();
     private static final FrameAnimationLoader missileLaunchLoader = new FrameAnimationLoader();
@@ -63,16 +79,16 @@ public class FrameAnimationFactory implements AnimationFactory {
         }
 
         //whiteSmokeBallLoader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "white_smoke_ball"));
-        //oneTankGunShellFireR_loader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "one_tank_gun_shell_fire_r"));
-        //oneTankGunShellFireTR_loader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "one_tank_gun_shell_fire_tr"));
-        //oneTankGunShellFireT_loader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "one_tank_gun_shell_fire_t"));
-        //oneTankGunShellFireTL_loader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "one_tank_gun_shell_fire_tl"));
-        //oneTankGunShellFireL_loader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "one_tank_gun_shell_fire_l"));
-        //oneTankGunShellFireBL_loader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "one_tank_gun_shell_fire_bl"));
-        //oneTankGunShellFireB_loader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "one_tank_gun_shell_fire_b"));
-        //oneTankGunShellFireBR_loader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "one_tank_gun_shell_fire_br"));
+        //oneTankGunShellFireEast_loader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "one_tank_gun_shell_fire_east"));
+        //oneTankGunShellFireNorthEast_loader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "one_tank_gun_shell_fire_north_east"));
+        //oneTankGunShellFireNorth_loader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "one_tank_gun_shell_fire_north"));
+        //oneTankGunShellFireNorthWest_loader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "one_tank_gun_shell_fire_north_west"));
+        //oneTankGunShellFireWest_loader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "one_tank_gun_shell_fire_west"));
+        //oneTankGunShellFireSouthWest_loader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "one_tank_gun_shell_fire_south_west"));
+        //oneTankGunShellFireSouth_loader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "one_tank_gun_shell_fire_south"));
+        //oneTankGunShellFireSouthEast_loader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "one_tank_gun_shell_fire_south_east"));
         //projectileExplosionLoader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "projectile_explosion"));
-        //tankProjectileLoader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "tank_projectile"));
+        //shellLoader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "shell"));
         //missileLoader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "missile"));
         //missileTrailLoader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "missile_trail"));
         //missileLaunchLoader.load(Gdx.files.internal(Constants.FOLDER_ANIMATIONS + "missile_launch"));
@@ -110,35 +126,35 @@ public class FrameAnimationFactory implements AnimationFactory {
             case ID_WHITE_SMOKE_BALL:
                 animation = whiteSmokeBallLoader.newInstance();
                 break;
-            case ID_ONE_TANK_GUN_SHELL_FIRE_R:
-                animation = oneTankGunShellFireR_loader.newInstance();
+            case ID_ONE_TANK_GUN_SHELL_FIRE_EAST:
+                animation = oneTankGunShellFireEast_loader.newInstance();
                 break;
-            case ID_ONE_TANK_GUN_SHELL_FIRE_TR:
-                animation = oneTankGunShellFireTR_loader.newInstance();
+            case ID_ONE_TANK_GUN_SHELL_FIRE_NORTH_EAST:
+                animation = oneTankGunShellFireNorthEast_loader.newInstance();
                 break;
-            case ID_ONE_TANK_GUN_SHELL_FIRE_T:
-                animation = oneTankGunShellFireT_loader.newInstance();
+            case ID_ONE_TANK_GUN_SHELL_FIRE_NORTH:
+                animation = oneTankGunShellFireNorth_loader.newInstance();
                 break;
-            case ID_ONE_TANK_GUN_SHELL_FIRE_TL:
-                animation = oneTankGunShellFireTL_loader.newInstance();
+            case ID_ONE_TANK_GUN_SHELL_FIRE_NORTH_WEST:
+                animation = oneTankGunShellFireNorthWest_loader.newInstance();
                 break;
-            case ID_ONE_TANK_GUN_SHELL_FIRE_L:
-                animation = oneTankGunShellFireL_loader.newInstance();
+            case ID_ONE_TANK_GUN_SHELL_FIRE_WEST:
+                animation = oneTankGunShellFireWest_loader.newInstance();
                 break;
-            case ID_ONE_TANK_GUN_SHELL_FIRE_BL:
-                animation = oneTankGunShellFireBL_loader.newInstance();
+            case ID_ONE_TANK_GUN_SHELL_FIRE_SOUTH_WEST:
+                animation = oneTankGunShellFireSouthWest_loader.newInstance();
                 break;
-            case ID_ONE_TANK_GUN_SHELL_FIRE_B:
-                animation = oneTankGunShellFireB_loader.newInstance();
+            case ID_ONE_TANK_GUN_SHELL_FIRE_SOUTH:
+                animation = oneTankGunShellFireSouth_loader.newInstance();
                 break;
-            case ID_ONE_TANK_GUN_SHELL_FIRE_BR:
-                animation = oneTankGunShellFireBR_loader.newInstance();
+            case ID_ONE_TANK_GUN_SHELL_FIRE_SOUTH_EAST:
+                animation = oneTankGunShellFireSouthEast_loader.newInstance();
                 break;
             case ID_PROJECTILE_EXPLOSION:
                 animation = projectileExplosionLoader.newInstance();
                 break;
-            case ID_TANK_PROJECTILE:
-                animation = tankProjectileLoader.newInstance();
+            case ID_SHELL:
+                animation = shellLoader.newInstance();
                 break;
             case ID_MISSILE:
                 animation = missileLoader.newInstance();
@@ -176,10 +192,11 @@ public class FrameAnimationFactory implements AnimationFactory {
             animation.setCenterY(y);
 
             animation.setInitialCenterX(x);
-            animation.setInitialCenterY(y);
 
             animation.setFinalCenterX(finalX);
             animation.setFinalCenterY(finalY);
+
+            animation.setInitialCenterY(y);
         } else {
             animation.setX(x);
             animation.setY(y);
--- core/src/com/gasis/rts/logic/object/combat/FireSource.java
+++ core/src/com/gasis/rts/logic/object/combat/FireSource.java
@@ -3,16 +3,38 @@ package com.gasis.rts.logic.object.combat;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.gasis.rts.logic.Renderable;
 import com.gasis.rts.logic.Updatable;
+import com.gasis.rts.logic.animation.Animation;
+import com.gasis.rts.logic.animation.AnimationFinishListener;
+import com.gasis.rts.logic.animation.complexanimation.MissileAnimation;
 import com.gasis.rts.logic.animation.complexanimation.ProjectileAnimation;
+import com.gasis.rts.logic.animation.frameanimation.FrameAnimationFactory;
 import com.gasis.rts.resources.Resources;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.gasis.rts.logic.object.unit.Unit.*;
+
 /**
  * A point from which shots are fired
  */
-public class FireSource implements Updatable, Renderable {
+public class FireSource implements Updatable, Renderable, AnimationFinishListener, TargetReachListener {
+
+    // all types of fire/projectiles
+    public static final byte FIRE_TYPE_MISSILE = 0;
+    public static final byte FIRE_TYPE_BULLET = 1;
+    public static final byte FIRE_TYPE_SHELL = 2;
+
+    // all scales of the projectile
+    public static final byte SMALL = 0;
+    public static final byte MEDIUM = 1;
+    public static final byte HEAVY = 2;
+
+    // the fire/projectile type of this fire source
+    protected byte fireType;
+
+    // the scale of this source's projectile
+    protected byte projectileScale;
 
     // coordinates of the source
     protected float x;
@@ -20,6 +42,42 @@ public class FireSource implements Updatable, Renderable {
 
     // animations used by the fire source
     protected List<ProjectileAnimation> animations = new ArrayList<ProjectileAnimation>();
+
+    // listeners for the reach of the target
+    protected List<TargetReachListener> targetReachListeners = new ArrayList<TargetReachListener>();
+
+    // how many guns are firing (only has effect on things that fire shells)
+    protected byte gunCount = 1;
+
+    /**
+     * Sets the gun count of the fire source
+     *
+     * @param gunCount can be 1 or 2
+     */
+    public void setGunCount(byte gunCount) {
+        if (gunCount != 1 && gunCount != 2) {
+            this.gunCount = 1;
+        } else {
+            this.gunCount = gunCount;
+        }
+    }
+
+    /**
+     * Gets the gun count of the fire source
+     * @return
+     */
+    public byte getGunCount() {
+        return gunCount;
+    }
+
+    /**
+     * Adds a new listener that listens for the reach of the target
+     *
+     * @param listener listener to add
+     */
+    public void addTargetReachListener(TargetReachListener listener) {
+        targetReachListeners.add(listener);
+    }
 
     /**
      * Sets the x coordinate of the fire source
@@ -56,13 +114,273 @@ public class FireSource implements Updatable, Renderable {
     }
 
     /**
+     * Gets the fire/projectile type of the fire source
+     * @return
+     */
+    public byte getFireType() {
+        return fireType;
+    }
+
+    /**
+     * Sets the fire/projectile type of the fire source
+     *
+     * @param fireType new fire type
+     */
+    public void setFireType(byte fireType) {
+        this.fireType = fireType;
+    }
+
+    /**
+     * Gets the scale of the projectile
+     * @return
+     */
+    public byte getProjectileScale() {
+        return projectileScale;
+    }
+
+    /**
+     * Sets the projectile scale
+     *
+     * @param projectileScale new projectile scale
+     */
+    public void setProjectileScale(byte projectileScale) {
+        this.projectileScale = projectileScale;
+    }
+
+    /**
+     * Gets called when a projectile reaches it's target
+     *
+     * @param targetX x coordinate of the target
+     * @param targetY y coordinate of the target
+     */
+    @Override
+    public void targetReached(float targetX, float targetY) {
+        for (TargetReachListener listener: targetReachListeners) {
+            listener.targetReached(targetX, targetY);
+        }
+    }
+
+    /**
+     * Fires a shot at the specified target coordinates
+     *
+     * @param facingDirection the direction the firing thing is facing
+     * @param targetX x coordinate of the target
+     * @param targetY y coordinate of the target
+     */
+    public void fire(byte facingDirection, float targetX, float targetY) {
+        ProjectileAnimation animation = createProjectileAnimation(facingDirection, targetX, targetY);
+        animation.addTargetReachedListener(this);
+
+        animations.add(animation);
+    }
+
+    /**
+     * Creates a new projectile animation
+     *
+     * @param facingDirection the direction the firing thing is facing
+     * @param targetX x coordinate of the target
+     * @param targetY y coordinate of the target
+     * @return
+     */
+    protected ProjectileAnimation createProjectileAnimation(byte facingDirection, float targetX, float targetY) {
+        ProjectileAnimation animation = null;
+        float scale = 0;
+
+        switch (projectileScale) {
+            case SMALL:
+                scale = 0.5f;
+                break;
+            case MEDIUM:
+                scale = 1f;
+                break;
+            case HEAVY:
+                scale = 1.5f;
+                break;
+            default:
+                scale = 1f;
+                break;
+        }
+
+        switch (fireType) {
+            case FIRE_TYPE_MISSILE:
+                animation = new MissileAnimation(scale, scale);
+                break;
+            case FIRE_TYPE_BULLET:
+                animation = createBulletAnimation(facingDirection, targetX, targetY);
+                break;
+            case FIRE_TYPE_SHELL:
+                animation = createShellAnimation(facingDirection, targetX, targetY);
+                break;
+            default:
+                throw new RuntimeException("Invalid fire/projectile type");
+        }
+
+        animation.setTrajectory(x, y, targetX, targetY);
+
+        return animation;
+    }
+
+    /**
+     * Creates a bullet animation
+     *
+     * @param facingDirection the direction the firing thing is facing
+     * @param targetX x coordinate of the target
+     * @param targetY y coordinate of the target
+     * @return
+     */
+    protected ProjectileAnimation createBulletAnimation(byte facingDirection, float targetX, float targetY) {
+        short fireAnimationId = -1;
+
+        switch (facingDirection) {
+            case NORTH:
+                fireAnimationId = FrameAnimationFactory.ID_BULLET_FIRE_NORTH;
+                break;
+            case NORTH_EAST:
+                fireAnimationId = FrameAnimationFactory.ID_BULLET_FIRE_NORTH_EAST;
+                break;
+            case EAST:
+                fireAnimationId = FrameAnimationFactory.ID_BULLET_FIRE_EAST;
+                break;
+            case SOUTH_EAST:
+                fireAnimationId = FrameAnimationFactory.ID_BULLET_FIRE_SOUTH_EAST;
+                break;
+            case SOUTH:
+                fireAnimationId = FrameAnimationFactory.ID_BULLET_FIRE_SOUTH;
+                break;
+            case SOUTH_WEST:
+                fireAnimationId = FrameAnimationFactory.ID_BULLET_FIRE_SOUTH_WEST;
+                break;
+            case WEST:
+                fireAnimationId = FrameAnimationFactory.ID_BULLET_FIRE_WEST;
+                break;
+            case NORTH_WEST:
+                fireAnimationId = FrameAnimationFactory.ID_BULLET_FIRE_NORTH_WEST;
+                break;
+            default:
+                throw new RuntimeException("Bad facing direction");
+        }
+
+        return new ProjectileAnimation(
+                FrameAnimationFactory.getInstance().create(FrameAnimationFactory.ID_BULLET),
+                FrameAnimationFactory.getInstance().create(fireAnimationId),
+                FrameAnimationFactory.getInstance().create(FrameAnimationFactory.ID_BULLET_RICOCHET)
+        );
+    }
+
+    /**
+     * Creates a shell animation
+     *
+     * @param facingDirection the direction the firing thing is facing
+     * @param targetX x coordinate of the target
+     * @param targetY y coordinate of the target
+     * @return
+     */
+    protected ProjectileAnimation createShellAnimation(byte facingDirection, float targetX, float targetY) {
+        short fireAnimationId = -1;
+
+        switch (facingDirection) {
+            case NORTH:
+                if (gunCount == 1) {
+                    fireAnimationId = FrameAnimationFactory.ID_ONE_TANK_GUN_SHELL_FIRE_NORTH;
+                } else {
+                    fireAnimationId = FrameAnimationFactory.ID_TWO_TANK_GUN_SHELL_FIRE_NORTH;
+                }
+
+                break;
+            case NORTH_EAST:
+                if (gunCount == 1) {
+                    fireAnimationId = FrameAnimationFactory.ID_ONE_TANK_GUN_SHELL_FIRE_NORTH_EAST;
+                } else {
+                    fireAnimationId = FrameAnimationFactory.ID_TWO_TANK_GUN_SHELL_FIRE_NORTH_EAST;
+                }
+
+                break;
+            case EAST:
+                if (gunCount == 1) {
+                    fireAnimationId = FrameAnimationFactory.ID_ONE_TANK_GUN_SHELL_FIRE_EAST;
+                } else {
+                    fireAnimationId = FrameAnimationFactory.ID_TWO_TANK_GUN_SHELL_FIRE_EAST;
+                }
+
+                break;
+            case SOUTH_EAST:
+                if (gunCount == 1) {
+                    fireAnimationId = FrameAnimationFactory.ID_ONE_TANK_GUN_SHELL_FIRE_SOUTH_EAST;
+                } else {
+                    fireAnimationId = FrameAnimationFactory.ID_TWO_TANK_GUN_SHELL_FIRE_SOUTH_EAST;
+                }
+
+                break;
+            case SOUTH:
+                if (gunCount == 1) {
+                    fireAnimationId = FrameAnimationFactory.ID_ONE_TANK_GUN_SHELL_FIRE_SOUTH;
+                } else {
+                    fireAnimationId = FrameAnimationFactory.ID_TWO_TANK_GUN_SHELL_FIRE_SOUTH;
+                }
+
+                break;
+            case SOUTH_WEST:
+                if (gunCount == 1) {
+                    fireAnimationId = FrameAnimationFactory.ID_ONE_TANK_GUN_SHELL_FIRE_SOUTH_WEST;
+                } else {
+                    fireAnimationId = FrameAnimationFactory.ID_TWO_TANK_GUN_SHELL_FIRE_SOUTH_WEST;
+                }
+
+                break;
+            case WEST:
+                if (gunCount == 1) {
+                    fireAnimationId = FrameAnimationFactory.ID_ONE_TANK_GUN_SHELL_FIRE_WEST;
+                } else {
+                    fireAnimationId = FrameAnimationFactory.ID_TWO_TANK_GUN_SHELL_FIRE_WEST;
+                }
+
+                break;
+            case NORTH_WEST:
+                if (gunCount == 1) {
+                    fireAnimationId = FrameAnimationFactory.ID_ONE_TANK_GUN_SHELL_FIRE_NORTH_WEST;
+                } else {
+                    fireAnimationId = FrameAnimationFactory.ID_TWO_TANK_GUN_SHELL_FIRE_NORTH_WEST;
+                }
+
+                break;
+            default:
+                throw new RuntimeException("Bad facing direction");
+        }
+
+        return new ProjectileAnimation(
+                FrameAnimationFactory.getInstance().create(FrameAnimationFactory.ID_SHELL),
+                FrameAnimationFactory.getInstance().create(fireAnimationId),
+                FrameAnimationFactory.getInstance().create(FrameAnimationFactory.ID_PROJECTILE_EXPLOSION)
+        );
+    }
+
+    /**
+     * Notifies the observer that the animation has finished
+     *
+     * @param animation the animation that just finished
+     */
+    @Override
+    public void finished(Animation animation) {
+        for (int i = 0; i < animations.size(); i++) {
+            if (animations.get(i) == animation) {
+                animations.remove(i);
+                break;
+            }
+        }
+    }
+
+    /**
      * Updates the state of the object
      *
      * @param delta time elapsed since the last update
      */
     @Override
     public void update(float delta) {
-
+        for (ProjectileAnimation animation: animations) {
+            if (animation != null) {
+                animation.update(delta);
+            }
+        }
     }
 
     /**
@@ -73,6 +391,10 @@ public class FireSource implements Updatable, Renderable {
      */
     @Override
     public void render(SpriteBatch batch, Resources resources) {
-
+        for (ProjectileAnimation animation: animations) {
+            if (animation != null) {
+                animation.render(batch, resources);
+            }
+        }
     }
 }
