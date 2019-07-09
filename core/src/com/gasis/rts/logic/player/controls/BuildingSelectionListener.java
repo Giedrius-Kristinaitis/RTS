ac4ed9011c1e83d0d0a35e38ceceb1e672fbe614
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-07-09 21:46:30 +0300

    Added buildingDeselected() method

--- core/src/com/gasis/rts/logic/player/controls/BuildingSelectionListener.java
+++ core/src/com/gasis/rts/logic/player/controls/BuildingSelectionListener.java
@@ -3,7 +3,7 @@ package com.gasis.rts.logic.player.controls;
 import com.gasis.rts.logic.object.building.Building;
 
 /**
- * Listens for building selection
+ * Listens for building selection events
  */
 public interface BuildingSelectionListener {
 
@@ -13,4 +13,9 @@ public interface BuildingSelectionListener {
      * @param building the selected building
      */
     void buildingSelected(Building building);
+
+    /**
+     * Called when the selected building gets deselected
+     */
+    void buildingDeselected();
 }
