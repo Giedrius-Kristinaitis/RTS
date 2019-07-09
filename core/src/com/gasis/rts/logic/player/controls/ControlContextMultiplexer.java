9a7a4e5763a0ccab92778af261b063ed1fe72fa0
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-07-09 22:36:13 +0300

    Created control context multiplexer which can consist of more than one control context

---
+++ core/src/com/gasis/rts/logic/player/controls/ControlContextMultiplexer.java
@@ -0,0 +1,22 @@
+package com.gasis.rts.logic.player.controls;
+
+import com.gasis.rts.logic.tech.Tech;
+
+import java.util.Map;
+
+/**
+ * Control context that consists of one or more control contexts
+ */
+public class ControlContextMultiplexer extends ControlContext {
+
+    /**
+     * Adds a control context to the multiplexer
+     *
+     * @param context context to add
+     */
+    public void addControlContext(ControlContext context) {
+        for (Map.Entry<String, Tech> tech: context.techs.entrySet()) {
+            techs.put(tech.getKey(), tech.getValue());
+        }
+    }
+}
