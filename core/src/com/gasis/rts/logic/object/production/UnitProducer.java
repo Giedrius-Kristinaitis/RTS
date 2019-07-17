5b28ae503ba3f1cbe70da1abda13b16138f18fa7
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-07-17 15:58:11 +0300

    Created unit producer abstraction

---
+++ core/src/com/gasis/rts/logic/object/production/UnitProducer.java
@@ -0,0 +1,15 @@
+package com.gasis.rts.logic.object.production;
+
+import com.gasis.rts.logic.object.unit.UnitLoader;
+
+/**
+ * Produces units
+ */
+public interface UnitProducer {
+
+    /**
+     * Queues up a unit to be produced
+     * @param unit loader of the unit
+     */
+    void queueUp(UnitLoader unit);
+}
