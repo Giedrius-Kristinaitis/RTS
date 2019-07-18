321f998caff2ff33a38873ed0908f5f81be4b4e5
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-07-18 22:08:04 +0300

    Fixed movement listener notifying

--- core/src/com/gasis/rts/logic/object/unit/Unit.java
+++ core/src/com/gasis/rts/logic/object/unit/Unit.java
@@ -162,6 +162,8 @@ public class Unit extends OffensiveGameObject implements AnimationFinishListener
         } else {
             moveOneBlockForward();
         }
+
+        notifyMovementStartListeners();
     }
 
     /**
