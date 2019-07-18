79f693744cb8c7b55b6a31d4c9d992365169580b
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-07-18 18:01:10 +0300

    Implemented Comparable interface

--- core/src/com/gasis/rts/math/Point.java
+++ core/src/com/gasis/rts/math/Point.java
@@ -3,7 +3,7 @@ package com.gasis.rts.math;
 /**
  * Represents a 2D point
  */
-public class Point {
+public class Point implements Comparable<Point> {
 
     // coordinates of the point
     public float x;
@@ -23,5 +23,22 @@ public class Point {
     public Point(float x, float y) {
         this.x = x;
         this.y = y;
+    }
+
+    /**
+     * Compares the point to another point
+     *
+     * @param p the other point
+     * @return
+     */
+    @Override
+    public int compareTo(Point p) {
+        if (x + y < p.x + p.y) {
+            return -1;
+        } else if (x + y > p.x + p.y) {
+            return 1;
+        }
+
+        return 0;
     }
 }
