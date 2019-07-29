b73b189c8868e3174443f11bec63d8d99a568ae8
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-07-29 14:32:20 +0300

    Fixed rotating gun's rotation and target distance calculation

--- core/src/com/gasis/rts/logic/object/combat/RotatingGun.java
+++ core/src/com/gasis/rts/logic/object/combat/RotatingGun.java
@@ -640,12 +640,12 @@ public class RotatingGun implements Updatable, Renderable, Rotatable, Aimable, D
         }
 
         if (target != null) {
-            rotateToDirection(CombatUtils.getFacingDirection(x + width / 2f, y + height / 2f, target.x, target.y));
+            rotateToDirection(CombatUtils.getFacingDirection(x, y, target.x, target.y));
 
-            if (rotatingToDirection == NONE && inSiegeMode && MathUtils.distance(x + width / 2f, target.x, y + height / 2f, target.y) <= offensiveSpecs.getSiegeModeAttackRange()) {
+            if (rotatingToDirection == NONE && inSiegeMode && MathUtils.distance(x, target.x, y, target.y) <= offensiveSpecs.getSiegeModeAttackRange()) {
                 firingLogic.target = target;
                 firingLogic.enqueueShots(inSiegeMode);
-            } else if (rotatingToDirection == NONE && !inSiegeMode && MathUtils.distance(x + width / 2f, target.x, y + height / 2f, target.y) <= offensiveSpecs.getAttackRange()) {
+            } else if (rotatingToDirection == NONE && !inSiegeMode && MathUtils.distance(x, target.x, y, target.y) <= offensiveSpecs.getAttackRange()) {
                 firingLogic.target = target;
                 firingLogic.enqueueShots(inSiegeMode);
             }
