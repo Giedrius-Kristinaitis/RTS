2cabe24849011dd24ec01f58abc00194fd3dc1bd
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-08-09 22:21:26 +0300

    Fixed a null pointer

--- core/src/com/gasis/rts/logic/pathfinding/PathFinder.java
+++ core/src/com/gasis/rts/logic/pathfinding/PathFinder.java
@@ -424,7 +424,7 @@ public class PathFinder implements PathFinderInterface {
     @Override
     public com.gasis.rts.math.Point getFinalDestination(Unit unit) {
         for (PathGroup group: groups) {
-            if (group.foundPaths.containsKey(unit)) {
+            if (group.foundPaths.containsKey(unit) && !group.foundPaths.get(unit).isEmpty()) {
                 return group.foundPaths.get(unit).getLast();
             }
         }
