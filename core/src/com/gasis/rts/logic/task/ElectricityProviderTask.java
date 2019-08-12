f3d4e97b285f0c4e584c99c71d41031a86a803cb
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-08-12 20:33:57 +0300

    Created electricity provider task

--- core/src/com/gasis/rts/logic/task/ElectricityProviderTask.java
+++ core/src/com/gasis/rts/logic/task/ElectricityProviderTask.java
@@ -1,4 +1,35 @@
 package com.gasis.rts.logic.task;
 
-public class ElectricityProviderTask {
+/**
+ * Provides electricity to a player
+ */
+public class ElectricityProviderTask extends ResourceProviderTask {
+
+    // the amount of provided electricity
+    private int amount;
+
+    /**
+     * Sets the amount of provided electricity
+     *
+     * @param amount amount of electricity
+     */
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    /**
+     * Executes the task
+     */
+    @Override
+    public void execute() {
+        player.getState().totalElectricity += amount;
+    }
+
+    /**
+     * Reverts the task
+     */
+    @Override
+    public void revert() {
+        player.getState().totalElectricity -= amount;
+    }
 }
