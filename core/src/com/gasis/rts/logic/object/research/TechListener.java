923429f69c089f3e600d06944e9e455f6fbb4308
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-08-16 21:44:20 +0300

    Created tech listener

---
+++ core/src/com/gasis/rts/logic/object/research/TechListener.java
@@ -0,0 +1,18 @@
+package com.gasis.rts.logic.object.research;
+
+import com.gasis.rts.logic.player.Player;
+import com.gasis.rts.logic.tech.Tech;
+
+/**
+ * Listens for tech research events
+ */
+public interface TechListener {
+
+    /**
+     * Called when a tech gets researched
+     *
+     * @param player the player the tech was applied to
+     * @param tech the researched tech
+     */
+    void techResearched(Player player, Tech tech);
+}
